9867a7481216a75a3e2db760bc014daaa2c7a8ff
gangyi89@gmail.com <gangyi89@gmail.com>
2020-11-14 19:53:08 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-gkwku-ny.us.auth0.com"
+    clientId="Rc2vsiFvDrttX5lyjh5JPS7ZRdbRoPMU"
+    redirectUri={window.location.origin}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
